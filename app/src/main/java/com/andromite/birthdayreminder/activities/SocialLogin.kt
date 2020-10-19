5639b1f901379db418c13d9013648268cb75b9d5
KhanStan99 <soubankhan3@gmail.com>
2020-10-19 20:56:17 +0530

    Pull Request Changes

--- app/src/main/java/com/andromite/birthdayreminder/activities/SocialLogin.kt
+++ app/src/main/java/com/andromite/birthdayreminder/activities/SocialLogin.kt
@@ -2,10 +2,12 @@ package com.andromite.birthdayreminder.activities
 
 import android.app.Activity
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import com.andromite.birthdayreminder.R
+import com.andromite.birthdayreminder.Utils.Constants.Companion.RC_SIGN_IN
 import com.andromite.birthdayreminder.Utils.SharedPrefrenceUtils
 import com.firebase.ui.auth.AuthUI
 import com.firebase.ui.auth.IdpResponse
@@ -16,41 +18,29 @@ import kotlinx.android.synthetic.main.activity_social_login.*
 
 class SocialLogin : AppCompatActivity() {
 
-    var RC_SIGN_IN = 1
-    val db = Firebase.firestore
+    private val db = Firebase.firestore
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_social_login)
 
-        //get current logged in user. returns null if not logged in.
-        var current_user = FirebaseAuth.getInstance().currentUser
-
-        // send to MainActivity if user is logged in
-//        if (current_user!=null) {
-//
-//            // save userUID to SP
-//            // send to HomeFragment
-//            Log.e("12345 current UID", current_user!!.uid)
-//            startActivity(Intent(this, MainActivity::class.java))
-//
-//        }
-
-
+        //region SignIn Process
         googleLogin.setOnClickListener {
 
-            // start signin process
             val providers = arrayListOf(
-                AuthUI.IdpConfig.GoogleBuilder().build())
+                AuthUI.IdpConfig.GoogleBuilder().build()
+            )
 
             startActivityForResult(
                 AuthUI.getInstance()
                     .createSignInIntentBuilder()
                     .setAvailableProviders(providers)
                     .build(),
-                RC_SIGN_IN)
+                RC_SIGN_IN
 
+            )
         }
+        //endregion
 
     }
 
@@ -61,12 +51,8 @@ class SocialLogin : AppCompatActivity() {
             val response = IdpResponse.fromResultIntent(data)
 
             if (resultCode == Activity.RESULT_OK) {
-                // Successfully signed in
                 val user = FirebaseAuth.getInstance().currentUser
-
-                var userUID = user!!.uid
-
-                Log.e("12345 UID after login", user!!.uid)
+                val userUID = user!!.uid
 
                 //add user to the collection in db
                 //create SharedPreference for user_id
@@ -76,21 +62,14 @@ class SocialLogin : AppCompatActivity() {
                     "email" to user.email
                 )
 
-
                 db.collection("users").document(userUID).set(userIdentity)
-                SharedPrefrenceUtils().saveSP(this,"googleuid",userUID)
+                SharedPrefrenceUtils().saveSP(this, "googleuid", userUID)
                 startActivity(Intent(this, MainActivity::class.java))
 
-
-                // ...
             } else {
-                // Sign in failed. If response is null the user canceled the
-                // sign-in flow using the back button. Otherwise check
-                // response.getError().getErrorCode() and handle the error.
-                // ...
+                Toast.makeText(this, "Login Failed, Please try again!", Toast.LENGTH_LONG).show()
             }
         }
-
     }
 
 }
