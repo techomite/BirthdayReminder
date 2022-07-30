48ffedf233783197655fe2660c0ea8e4a7ea8540
Mohammed Faizan Hussain <faizan@brochill.com>
2022-07-31 05:11:35 +0530

    [upgrade] build.gradle file

--- app/src/main/java/com/andromite/birthdayreminder/db/BirthdayDao.kt
+++ app/src/main/java/com/andromite/birthdayreminder/db/BirthdayDao.kt
@@ -7,21 +7,21 @@ import androidx.room.Query
 @Dao
 interface BirthdayDao {
 
-    @Insert
-    suspend fun addBirthday(birthday : Birthday)
+//    @Insert
+//    suspend fun addBirthday(birthday : Birthday)
 
-    @Query("SELECT * FROM birthday ORDER BY id DESC" )
-    suspend fun getAllBirthday() : List<Birthday>
-
-    @Query("UPDATE birthday SET person_name=:personName, date=:date, event=:event, isImportant=:important, notes=:notes, profilePic=:profilePic WHERE id=:id")
-    suspend fun updateBirthday(
-        personName: kotlin.String,
-        date: kotlin.String,
-        event: kotlin.Int,
-        important: kotlin.Boolean,
-        notes: kotlin.String,
-        profilePic: kotlin.ByteArray?,
-        id: kotlin.Int
-    )
+//    @Query("SELECT * FROM birthday ORDER BY id DESC" )
+//    suspend fun getAllBirthday() : List<Birthday>
+//
+//    @Query("UPDATE birthday SET person_name=:personName, date=:date, event=:event, isImportant=:important, notes=:notes, profilePic=:profilePic WHERE id=:id")
+//    suspend fun updateBirthday(
+//        personName: kotlin.String,
+//        date: kotlin.String,
+//        event: kotlin.Int,
+//        important: kotlin.Boolean,
+//        notes: kotlin.String,
+//        profilePic: kotlin.ByteArray?,
+//        id: kotlin.Int
+//    )
 
 }
